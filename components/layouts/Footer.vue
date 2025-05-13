60d9dc9558c7f117c5785709c4b659dec5e81b31
脚本小哥 <jorbenzhu@gmail.com>
2025-05-14 00:24:31 +0800

    misc: Update Footer.vue

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -8,17 +8,17 @@ const { title, email, telegram, blog, twitter, mastodon, github } = useAppConfig
   <section class="md:pt-6">
     <div class="container flex flex-col items-center py-8 mx-auto sm:flex-row">
       <a
-        href="https://sink.cool"
+        href="https://urix.cc"
         class="text-xl font-black leading-none text-gray-900 select-none dark:text-gray-100 logo"
         :title="title"
       >{{ title }}</a>
       <a
         class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
-        href="https://urix.cc"
+        href="https://sink.cool"
         target="_blank"
         title="URIx.cc"
       >
-        &copy; {{ new Date().getFullYear() }} URIx.CC
+        Powered by Sink.
       </a>
       <a 
         href="https://beian.miit.gov.cn/" 
