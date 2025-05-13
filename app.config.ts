a4dc99eed563621b9f0510e15b6d1bb5d025938f
脚本小哥 <jorbenzhu@gmail.com>
2025-05-14 00:08:52 +0800

    misc: Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   telegram: 'https://sink.cool/telegram',
   mastodon: 'https://sink.cool/mastodon',
   blog: 'https://sink.cool/blog',
-  description: '',
+  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
