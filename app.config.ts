a87263bc93e3f3bbe04f327c18f252d16231f1f7
脚本小哥 <jorbenzhu@gmail.com>
2025-05-14 00:04:15 +0800

    misc: Update app.config.ts, modify title and description

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,12 @@
 export default defineAppConfig({
-  title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
+  title: 'URIX-短链接',
+  email: 'jorbenzhu@gmail.com',
+  github: 'https://github.com/jorben/sink',
   twitter: 'https://sink.cool/kai',
   telegram: 'https://sink.cool/telegram',
   mastodon: 'https://sink.cool/mastodon',
   blog: 'https://sink.cool/blog',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  description: '',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
